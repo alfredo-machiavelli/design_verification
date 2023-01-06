e55a1f020e60178df6ef6475350618876c088410
Alfredo Machiavelli <120282637+alfredo-machiavelli@users.noreply.github.com>
2023-01-06 04:43:47 -0600

    set membership

--- ch12_random_constraints.v
+++ ch12_random_constraints.v
@@ -37,6 +37,7 @@ endclass
   
   here MyBus inherits all of the random variables + constraints of the Bus class 
   adds a rand var called atype used to control the addrss range using another constraint
+  addr_range constraint  - select one of 3 range constraints depending on the random val of atype
   
 */
 typedef enum {low, mid, high} AddrType;
@@ -54,20 +55,98 @@ class MyBus extends Bus;
 endclass
     
     
+//with construct - declares additional constraints in-line with the call to randomize()
+
+task exercise_bus (MyBus bus);
+  
+  int res;
+  // EXAMPLE 1: restrict to low addresses
+  res = bus.randomize() with {atype == low;};
+  // EXAMPLE 2: restrict to address between 10 and 20
+  res = bus.randomize() with {10 <= addr && addr <= 20;};
+  // EXAMPLE 3: restrict data values to powers-of-two
+  res = bus.randomize() with {data & (data - 1) == 0;};
+  
+endtask
     
+//disable constraints on random variables    
+ 
+task exercise_illegal(MyBus bus, int cycles);
+  
+  int res;
+  // Disable word alignment constraint
+  // constraint_mode() disables/enables constraint block on an object
+  
+  bus.word_align.constraint_mode(0);
+  
+  repeat (cycles) begin
     
+    // CASE 1: restrict to small addresses.
+    res = bus.randomize() with {addr[0] || addr[1];};
+      ...
+  end
+  
+  // Re-enable word alignment constraint
+  bus.word_align.constraint_mode(1);
+  
+endtask
+
+//RANDOM VARIABLES
     
+rand bit [7:0] y;         //values are uniformly distributed over this range
+// 8 -bit unsigned integer w/ range 0 to 255; if unconstrained, this var shall be assigned any value in the range 0 to 255 w/ equal probability (1/256)
+
+randc bit [1:0] y;        //randc = randomly cycle through all the values in a random permutation of their declared range
+/*
+  can take on values 0,1,2,3 (range 0-3) so..
+  initial permutation: 0 -> 3 -> 2 -> 1
+  next permutation: 2 -> 1 -> 3 -> 0
+  next permutation: 2 -> 0 -> 1 -> 3
+*/
+
+// CONSTRAINT BLOCKS     
     
+   // class declaration
+      class XYPair;
+        rand integer x, y; 
+        constraint c;
+      endclass
+  // external constraint body declaration
+      constraint XYPair::c { x < y; }
     
+//SET MEMBERSHIP
+  //equal probability of being chosen by the inside operator
+
+  rand integer x, y, z;
+  constraint c1 {x inside {3, 5, [9:15], [24:32], [y:2*y], z};}
+
+  rand integer a, b, c;
+  constraint c2 {a inside {b, c};}              //inside is bidirectional
+
+  integer fives[0:3] = { 5, 10, 15, 20 };
+  rand integer v;
+  constraint c3 { v inside fives; }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+//DISTRIBUTION
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
